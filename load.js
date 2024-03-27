d9f20a07bc8650dde41d7dac76ab1403a0db7e21
Dominique Hazael-Massieux <dom@w3.org>
2024-03-27 16:57:45 +0100

    Add semi-automatic interop report for webrtc amendments

--- load.js
+++ load.js
@@ -1,6 +1,6 @@
 let wptData;
 const resultsBody = document.querySelector(".results tbody");
-
+var { promise: testResultsLoaded, resolve: testLoadRes} = Promise.withResolvers();
 
 let runIds;
 fetch("https://wpt.fyi/api/runs?label=master")
@@ -12,20 +12,33 @@ fetch("https://wpt.fyi/api/runs?label=master")
   .then(r => r.json())
   .then(data => {
     wptData = data;
-    return fetch("annotations" + (window.spec ? "-" + window.spec : "") + ".json");
+    return fetch("annotations" + (window.spec ? "-" + window.spec : "") + (window.specFilter ?? "") + ".json");
   })
   .then(r => r.json())
-  .then(annotations => {
+  .then(async annotations => {
+    let tests, untested;
+    if (window.relevantTests) {
+      tests = await window.relevantTests;
+      untested = new Set(tests);
+    }
     wptData.results
       .forEach(r => {
         const testName = r.test.slice(1);
+	// allow to filter tests by setting window.relevantTests in calling file
+	if (tests) {
+	  if (!tests.includes(testName)) {
+	    return;
+	  } else {
+	    untested.delete(testName);
+	  }
+	}
         const annotation = annotations[testName] || {};
         const tr = document.createElement("tr");
         const testTd = document.createElement("th");
         const link = document.createElement("a");
         link.href = "https://wpt.fyi/results" + r.test + "?run_ids=" + runIds.join(",");
         link.textContent = testName;
-        testTd.appendChild(link)
+        testTd.appendChild(link);
         tr.appendChild(testTd);
         let fullImpl = 0;
         r.legacy_status.forEach((s,i) => {
@@ -59,4 +72,5 @@ fetch("https://wpt.fyi/api/runs?label=master")
         tr.appendChild(commentTd);
         resultsBody.appendChild(tr);
       });
-  });
+    console.error("Tests selected but whose results are not found:", untested);
+  }).then(testLoadRes);
